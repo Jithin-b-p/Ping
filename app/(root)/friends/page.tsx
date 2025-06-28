fd4be7a80e4950e321c7ec5cd0d74a414095fee6
unknown <jithinbp007@gmail.com>
2025-06-28 15:30:43 +0530

    created itemlist and added to friends route

--- app/(root)/friends/page.tsx
+++ app/(root)/friends/page.tsx
@@ -1,9 +1,14 @@
+import ItemList from "@/components/shared/item-list/ItemList";
 import React from "react";
 
 type Props = {};
 
 const FriendsPage = (props: Props) => {
-  return <div>FriendsPage</div>;
+  return (
+    <div>
+      <ItemList title="Friends">FriendsPage</ItemList>
+    </div>
+  );
 };
 
 export default FriendsPage;
